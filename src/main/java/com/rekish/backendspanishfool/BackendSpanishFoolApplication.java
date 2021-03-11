2fdfb07e17746b2032d8a6f9768f1b0942b171ee
Fiodar Rekish <fed_rekish@hotmail.com>
2021-03-11 22:11:34 +0300

    Initial commit

---
+++ src/main/java/com/rekish/backendspanishfool/BackendSpanishFoolApplication.java
@@ -0,0 +1,13 @@
+package com.rekish.backendspanishfool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpanishFoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpanishFoolApplication.class, args);
+    }
+
+}
